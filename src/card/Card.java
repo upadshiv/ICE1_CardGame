459e6c98bedcd5fccd4dfe1178d9f5d53e4ec985
shivupadhyay <upadshiv@sheridancollege.ca>
2025-09-18 21:01:15 -0400

    ICE 1 updated commit

--- src/card/Card.java
+++ src/card/Card.java
@@ -4,7 +4,7 @@
  */
 package card;
 /**
- * A class that models playing card Objects. Cards have 
+ * a class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -7,7 +7,7 @@ package card;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
+/**new code
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
